6e149b8a931b0a85c5e8084368e97bb909c66af7
rnakai <rnakai@student.42tokyo.jp>
2020-12-14 22:47:28 +0900

    fscanfの使い方を勉強

---
+++ exam03study/new_func_study.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int		main(void)
+{
+	FILE	*fp;
+	int		ret;
+	int		width;
+	int		height;
+	char	background;
+
+	fp = fopen("ex1", "r");
+	if (fp == NULL)
+	{
+		perror("Error:");
+		return (-1);
+	}
+	while ((ret = fscanf(fp, "%d %d %c\n", &width, &height, &background)) != EOF)
+	{
+		printf("width = %d\n", width);
+		printf("height = %d\n", height);
+		printf("background = %c\n", background);
+	}
+
+	printf("\nret = %d\n", ret);
+
+	fclose(fp);
+}
